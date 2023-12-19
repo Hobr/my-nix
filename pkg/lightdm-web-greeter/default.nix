4f94eff953672b3f8bd3e0a4501414b164268c82
Hobr <mail@hobr.site>
2023-12-19 18:33:30 +0800

    meta

--- pkg/lightdm-web-greeter/default.nix
+++ pkg/lightdm-web-greeter/default.nix
@@ -1,16 +1,21 @@
-{ stdenv, fetchFromGitHub, ... } @ args:
+{ lib, fetchFromGitHub, stdenv }:
 
 stdenv.mkDerivation rec {
   pname = "lightdm-web-greeter";
   version = "3.5.2";
 
-  src = fetchFromGitHub ({
+  src = fetchFromGitHub {
     owner = "JezerM";
     repo = "web-greeter";
     rev = version;
     fetchSubmodules = true;
     sha256 = "sha256-m20M4+3zsH40hTpMJG9cyIjXp0xcCUBS+cCiRVLXFqM=";
-  });
+  };
 
-  enableParallelBuilding = true;
+  meta = with lib; {
+    homepage = "https://github.com/JezerM/web-greeter";
+    description = "A modern, visually appealing greeter for LightDM.";
+    platforms = platforms.linux;
+    license = licenses.gpl3;
+  };
 }

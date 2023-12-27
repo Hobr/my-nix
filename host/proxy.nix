cd180ddfff436bf33ac819b9ca3ffc583e99b5d7
Hobr <mail@hobr.site>
2023-12-27 17:04:19 +0800

    clash & proxychains

--- host/proxy.nix
+++ host/proxy.nix
@@ -2,16 +2,23 @@
 {
   programs.proxychains = {
     enable = true;
+    package = pkgs.proxychains-ng;
     # 安静模式
     quietMode = true;
 
     # 代理
     proxies = {
-      myclash = {
+      ClashSocket = {
         enable = true;
         type = "socks5";
         host = "127.0.0.1";
         port = 7891;
+      };
+      ClashHttp = {
+        enable = true;
+        type = "http";
+        host = "127.0.0.1";
+        port = 7890;
       };
     };
   };
@@ -20,40 +27,34 @@
   environment.systemPackages = [ pkgs.unstable.clash-meta ];
 
   # 守护进程
-  systemd.user.services.Mihomo = {
-    Unit = {
-      Description = "Mihomo daemon, another Clash Kernel.";
+  systemd.services.Clash = {
+    unitConfig = {
+      Description = "Clash daemon";
+      After = "NetworkManager.service";
     };
-    Service = {
+    serviceConfig = {
       Type = "simple";
       LimitNPROC = "500";
       LimitNOFILE = "1000000";
       CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME";
       AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME";
-      Restart = "always";
-      ExecStartPre = "${pkgs.coreutils}/bin/sleep 1s";
-      ExecStartPre = "${pkgs.wget}/bin/wget -O /home/hobr/.config/mihomo/config.yaml https://conv.trafficmanager.net/sub?target=clash&url=https://yy-node.trafficmanager.net/link/ArDF5nbkrwv0dqcD?sub=2&udp=true&extend=1&filename=yy";
-      ExecStart = "${pkgs.mihomo}/bin/mihomo -d /home/hobr/.config/mihomo";
+      ExecStartPre = "${pkgs.wget}/bin/wget -O /etc/clash/config.yaml 'https://conv.trafficmanager.net/sub?target=clash&url=https://yy-node.trafficmanager.net/link/ArDF5nbkrwv0dqcD?sub=2&udp=true&extend=1&filename=yy'";
+      ExecStart = "${pkgs.clash-meta}/bin/clash-meta -d /etc/clash";
       ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
-    };
-    Install = {
-      WantedBy = [ "multi-user.target" ];
     };
   };
 
   # 定时更新订阅
-  systemd.user.timers.SubUpdate = {
-    Unit = {
-      Description = "Mihomo subscription update.";
+  systemd.timers.SubUpdate = {
+    unitConfig = {
+      Description = "Clash subscription update.";
       After = "NetworkManager.service";
     };
-    Timer = {
+    timerConfig = {
       OnCalendar = "hourly";
       Persistent = "true";
-      Unit = "Mihomo.service";
+      Unit = "Clash.service";
     };
-    Install = {
-      WantedBy = [ "timers.target" "multi-user.target" ];
-    };
+    wantedBy = [ "timers.target" "multi-user.target" ];
   };
 }

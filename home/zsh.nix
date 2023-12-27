fb8f348ad289ac2c01050831b93e66d1a13214bc
Hobr <mail@hobr.site>
2023-12-27 21:31:57 +0800

    zsh & powerlevel10k

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,14 +1,23 @@
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
 
-    # Plugin
+    # 插件
     enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    # OMZ
-    oh-my-zsh = {
-      enable = true;
-    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./config;
+        file = "p10k.zsh";
+      }
+    ];
   };
 }

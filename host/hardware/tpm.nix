c20707a807bf010bd3baf7ba5492b439c7d46626
Hobr <mail@hobr.site>
2023-12-29 00:14:29 +0800

    tpm

--- host/hardware/tpm.nix
+++ host/hardware/tpm.nix
@@ -1,8 +1,15 @@
+{ pkgs, lib, ... }:
 {
   security.tpm2 = {
     enable = true;
-    # abrmd.enable = true;
+    abrmd.enable = true;
     tctiEnvironment.enable = true;
     pkcs11.enable = true;
   };
+
+  environment.systemPackages = lib.mkAfter [ pkgs.tpm2-tools ];
+  # 用户组
+  users.users.kanade.extraGroups = [ "tss" ];
+  boot.initrd.availableKernelModules = lib.mkAfter [ "tpm_crb" ];
+  boot.initrd.systemd.enableTpm2 = true;
 }

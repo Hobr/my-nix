3982fa5aeb42ca83e4f98349d6ea592407580fa0
Hobr <mail@hobr.site>
2023-12-31 00:21:16 +0800

    hyprland & greetd

--- host/system/launcher.nix
+++ host/system/launcher.nix
@@ -1,24 +1,54 @@
+{ pkgs, ... }:
 {
+  # Greet
+  environment.etc."greetd/environments".text = ''
+    Hyprland
+  '';
+
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "Hyprland";
+        user = "kanade";
+      };
+      default_session = initial_session;
+    };
+  };
+
+  # 自动登录
+#  services.xserver.displayManager.autoLogin = {
+#    enable = true;
+#    user = "kanade";
+#  };
+#  systemd.services."getty@tty1".enable = false;
+#  systemd.services."autovt@tty1".enable = false;
+
+  # Hyprland
   programs.hyprland = {
     enable = true;
     enableNvidiaPatches = true;
+    xwayland.enable = true;
   };
 
-  # GDM
-  services.xserver.enable = true;
-  services.xserver.displayManager = {
-    gdm = {
-      enable = true;
-      wayland = true;
-    };
-
-    # 自动登录
-    autoLogin = {
-      enable = true;
-      user = "kanade";
-    };
+  # XDG Portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  # seatd
+  systemd.services = {
+    seatd = {
+      enable = true;
+      description = "Seat management daemon";
+      script = "${pkgs.seatd}/bin/seatd -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "1";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
 }

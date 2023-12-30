0e03d408ddf4780dbd4d856b9e8a8bc092d17288
Hobr <mail@hobr.site>
2023-12-30 16:59:06 +0800

    no cacheix

--- host/global/nix.nix
+++ host/global/nix.nix
@@ -23,8 +23,7 @@
 
       # 镜像
       trusted-users = [ "kanade" ];
-      substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" "http://mirrors.ustc.edu.cn/nix-channels/store/" "https://cache.nixos.org" "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" "http://mirrors.ustc.edu.cn/nix-channels/store/" "https://cache.nixos.org" ];
     };
 
     # GC

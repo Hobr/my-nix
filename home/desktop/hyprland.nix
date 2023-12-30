56d80c30de4919ca70d89c375e5dcc50e34499e2
Hobr <mail@hobr.site>
2023-12-30 16:48:17 +0800

    hyprland config

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -17,11 +17,14 @@ in
   imports = [ inputs.hyprland.homeManagerModules.default ];
 
   home.packages = with pkgs; [
+    qt6.qtwayland
     qt6Packages.qt6ct
+
+    libsForQt5.qt5.qtwayland
     libsForQt5.qt5ct
 
-    qt6.qtwayland
-    libsForQt5.qt5.qtwayland
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
   ];
 
   # 环境变量
@@ -34,7 +37,7 @@ in
 
     # 桌面
     XCURSOR_SIZE = 24;
-    QT_QPA_PLATFORMTHEME = "qt6ct";
+    QT_QPA_PLATFORMTHEME = "gtk3";
     QT_QPA_PLATFORM = "wayland";
     SDL_VIDEODRIVER = "wayland";
     XDG_SESSION_TYPE = "wayland";
@@ -42,12 +45,6 @@ in
 
   wayland.windowManager.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = [
-      inputs.xdg-desktop-portal-hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal
-    ];
 
     settings = {
       exec-once = [ "${xdgstart}/bin/xdgstart" ];
@@ -77,92 +74,89 @@ in
           passes = 1;
           vibrancy = 0.1696;
         };
-
         drop_shadow = true;
         shadow_range = 4;
         shadow_render_power = 3;
         col.shadow = "rgba(1a1a1aee)";
       };
 
-      animations {
-      enabled = true
-
+      animations = {
+        enabled = true;
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
 
-      animation = [
-        "windows, 1, 7, myBezier"
-        "windowsOut, 1, 7, default, popin 80%"
-        "border, 1, 10, default"
-        "borderangle, 1, 8, default"
-        "fade, 1, 7, default"
-        "workspaces, 1, 6, default"
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      master = {
+        new_is_master = true;
+      };
+
+      gestures = {
+        workspace_swipe = false;
+      };
+
+      misc = {
+        force_default_wallpaper = -1;
+      };
+
+      windowrulev2 = "nomaximizerequest, class:.*";
+
+      # 快捷键
+      "$mainMod" = "SUPER";
+
+      bind = [
+        "$mainMod, Q, exec, alacritty"
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, P, pseudo,"
+        "$mainMod, J, togglesplit,"
+
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+
+        "$mainMod, mouse_up, workspace, e+1"
+        "$mainMod, mouse_down, workspace, e-1"
+      ];
+
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
       ];
     };
-
-    dwindle {
-    pseudotile = true;
-    preserve_split = true;
   };
-
-  master {
-  new_is_master = true;
-};
-
-gestures {
-workspace_swipe = false;
-};
-
-misc {
-force_default_wallpaper = -1;
-};
-
-windowrulev2 = "nomaximizerequest, class:.*";
-
-# 快捷键
-"$mainMod" = "SUPER";
-
-bind = [
-"$mainMod, Q, exec, alacritty"
-"$mainMod, C, killactive,"
-"$mainMod, M, exit,"
-"$mainMod, V, togglefloating,"
-"$mainMod, P, pseudo,"
-"$mainMod, J, togglesplit,"
-
-"$mainMod, left, movefocus, l"
-"$mainMod, right, movefocus, r"
-"$mainMod, up, movefocus, u"
-"$mainMod, down, movefocus, d"
-
-"$mainMod, 1, workspace, 1"
-"$mainMod, 2, workspace, 2"
-"$mainMod, 3, workspace, 3"
-"$mainMod, 4, workspace, 4"
-"$mainMod, 5, workspace, 5"
-"$mainMod, 6, workspace, 6"
-"$mainMod, 7, workspace, 7"
-"$mainMod, 8, workspace, 8"
-"$mainMod, 9, workspace, 9"
-"$mainMod, 0, workspace, 10"
-
-"$mainMod SHIFT, 1, movetoworkspace, 1"
-"$mainMod SHIFT, 2, movetoworkspace, 2"
-"$mainMod SHIFT, 3, movetoworkspace, 3"
-"$mainMod SHIFT, 4, movetoworkspace, 4"
-"$mainMod SHIFT, 5, movetoworkspace, 5"
-"$mainMod SHIFT, 6, movetoworkspace, 6"
-"$mainMod SHIFT, 7, movetoworkspace, 7"
-"$mainMod SHIFT, 8, movetoworkspace, 8"
-"$mainMod SHIFT, 9, movetoworkspace, 9"
-
-"$mainMod, mouse_up, workspace, e+1"
-"$mainMod, mouse_down, workspace, e-1"
-];
-
-bindm = [
-"$mainMod, mouse:272, movewindow"
-"$mainMod, mouse:273, resizewindow"
-];
-};
-};
 }

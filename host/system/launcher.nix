bd59d24444a223d78dc5d4c6e3f59cd8e3a81447
Hobr <mail@hobr.site>
2023-12-30 16:23:13 +0800

    hyprland

--- host/system/launcher.nix
+++ host/system/launcher.nix
@@ -1,17 +1,27 @@
+{ inputs, pkgs, ... }:
 {
+  imports = [ inputs.hyprland.nixosModules.default ];
+
   services.xserver.enable = true;
 
   services.xserver.displayManager = {
-    #defaultSession = "Hyprland";
+    # GDM
     gdm = {
       enable = true;
       wayland = true;
     };
+
+    # 自动登录
+    autoLogin = {
+      enable = true;
+      user = "kanade";
+    };
+
   };
 
-  # 自动启动
-  #services.xserver.displayManager.autoLogin.enable = true;
-  #services.xserver.displayManager.autoLogin.user = "kanade";
+  # Hyprland
+  programs.hyprland.enable = true;
+
   #systemd.services."getty@tty1".enable = false;
   #systemd.services."autovt@tty1".enable = false;
 }

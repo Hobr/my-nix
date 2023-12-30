b4e7d9366bbceff0f1b9a9d82d6f69e308f2deff
Hobr <mail@hobr.site>
2023-12-31 00:01:03 +0800

    env

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -25,6 +25,10 @@
         "GBM_BACKEND,nvidia-drm"
         "__GLX_VENDOR_LIBRARY_NAME,nvidia"
         "WLR_NO_HARDWARE_CURSORS,1"
+        "QT_QPA_PLATFORM,wayland"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "MOZ_ENABLE_WAYLAND,1"
         #"NIXOS_OZONE_WL,1"
       ];
 

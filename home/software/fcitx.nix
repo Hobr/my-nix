754d28d445c65137bda378196a354fc85c23bcfe
Hobr <mail@hobr.site>
2024-01-07 00:07:58 +0800

    format

--- home/software/fcitx.nix
+++ home/software/fcitx.nix
@@ -4,6 +4,12 @@
     enabled = "fcitx5";
 
     # Fcitx5
-    fcitx5.addons = with pkgs; [ fcitx5-gtk fcitx5-configtool fcitx5-rime fcitx5-chinese-addons fcitx5-anthy ];
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      fcitx5-configtool
+      fcitx5-rime
+      fcitx5-chinese-addons
+      fcitx5-anthy
+    ];
   };
 }

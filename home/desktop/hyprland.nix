39b4dc0f6249de94035b70f9b839c5dab04dab80
Hobr <mail@hobr.site>
2024-01-11 21:06:51 +0800

    workspace keybind

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -164,8 +164,8 @@ in
         "$mainMod SHIFT, 8, movetoworkspace, 8"
         "$mainMod SHIFT, 9, movetoworkspace, 9"
 
-        "$mainMod, mouse_up, workspace, e+1"
-        "$mainMod, mouse_down, workspace, e-1"
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
 
         # Pipewire
         ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+"

deba95defcebcb0e57f8a64b16d95ec522b6f2aa
Hobr <mail@hobr.site>
2024-01-23 13:29:36 +0800

    git ssh proxy

--- home/tool/ssh.nix
+++ home/tool/ssh.nix
@@ -1,7 +1,17 @@
+{ pkgs, lib, ... }:
 {
   programs.ssh = {
     enable = true;
+    extraConfig = ''
+      Host github.com
+        Hostname ssh.github.com
+        Port 443
+        ProxyCommand ${pkgs.unstable.netcat-gnu}/bin/nc -v -X 5 -x 127.0.0.1:7891 %h %p
+    '';
   };
 
   services.ssh-agent.enable = true;
+
+  # 代理
+  home.packages = with pkgs.unstable; lib.mkAfter [ netcat-gnu ];
 }

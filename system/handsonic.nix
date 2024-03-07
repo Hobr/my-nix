899499c25b55dfbfc9c363cce19292621d48c6db
Hobr <mail@hobr.site>
2024-03-07 14:36:54 +0800

    host

--- system/handsonic.nix
+++ system/handsonic.nix
@@ -7,5 +7,13 @@
     inputs.nh.nixosModules.default
 
     ./common
+
+    ./handsonic/kernel.nix
+    ./handsonic/filesystem.nix
+    ./handsonic/gpu.nix
+    ./handsonic/hardware.nix
+    ./handsonic/user.nix
+    ./handsonic/power.nix
+    ./handsonic/shell.nix
   ];
 }

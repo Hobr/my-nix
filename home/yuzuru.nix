21c76c3127e3cd71ca327204e12233efe06a68a7
Hobr <mail@hobr.site>
2024-03-07 21:40:18 +0800

    yuzuru

--- home/yuzuru.nix
+++ home/yuzuru.nix
@@ -1,1 +1,36 @@
 
+{ outputs, ... }:
+{
+  imports = [
+    ./common
+    ./yuzuru
+  ];
+
+  nixpkgs = {
+    # Overlays
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+
+    # 配置
+    config.allowUnfree = true;
+  };
+
+  home = {
+    username = "yuzuru";
+    homeDirectory = "/home/yuzuru";
+    stateVersion = "23.11";
+  };
+
+  # 文档
+  manual = {
+    manpages.enable = false;
+    html.enable = false;
+    json.enable = false;
+  };
+
+  programs.home-manager.enable = true;
+  systemd.user.startServices = "sd-switch";
+}

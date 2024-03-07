81f05d5bb1664f2f5383da04c2c255b1ea00a1d5
Hobr <mail@hobr.site>
2024-03-07 20:37:08 +0800

    yuzuru home

---
+++ home/yuzuru/hypr.nix
@@ -0,0 +1,15 @@
+{
+  wayland.windowManager.hyprland = {
+    # 设置
+    settings = {
+      monitor = [
+        "eDP-1,2160x1350@60,auto,1.25"
+      ];
+
+      env = [
+        # 鼠标大小
+        "XCURSOR_SIZE,20"
+      ];
+    };
+  };
+}

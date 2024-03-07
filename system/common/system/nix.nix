0fbe8ef16c83854f0266f156089e833bddadb575
Hobr <mail@hobr.site>
2024-03-07 21:15:08 +0800

    mirror

--- system/common/system/nix.nix
+++ system/common/system/nix.nix
@@ -28,7 +28,7 @@
       substituters = [
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
       ];
     };

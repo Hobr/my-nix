b7f11ea172d08b7d75408bae784c3c76f06b76d0
Hobr <mail@hobr.site>
2024-03-09 15:15:00 +0800

    restruct

--- user/common/default.nix
+++ user/common/default.nix
@@ -1,49 +1,7 @@
 {
   imports = [
-    # 剪切板
-    ./desktop/clipboard.nix
-    # 桌面
-    ./desktop/hypr.nix
-    # 锁屏
-    ./desktop/lock.nix
-    # 注销
-    ./desktop/logout.nix
-    # 消息
-    ./desktop/notice.nix
-    # 启动器
-    ./desktop/rofi.nix
-    # 终端
-    ./desktop/terminal.nix
-    # 主题
-    ./desktop/theme.nix
-    # 任务栏
-    ./desktop/waybar.nix
-
-    # 音频
-    ./software/audio.nix
-    # 下载
-    ./software/download.nix
-    # 输入法
-    ./software/fcitx.nix
-    # 浏览器
-    ./software/firefox.nix
-    # 即时通讯
-    ./software/im.nix
-    # 图像
-    ./software/image.nix
-    # 管理
-    ./software/manage.nix
-    # NeoVim
-    ./software/nvim.nix
-    # OBS-Studio
-    ./software/obs.nix
-    # 邮箱
-    ./software/thunderbird.nix
-    # 视频
-    ./software/video.nix
-    # VS Code
-    ./software/vscode.nix
-
+    # Nix
+    ./tool/sync.nix
     # 硬盘
     ./tool/disk.nix
     # Git
@@ -55,24 +13,71 @@
     # 同步
     ./tool/sync.nix
 
+    # 桌面
+    ./desktop/hypr.nix
+    # 任务栏
+    ./desktop/waybar.nix
+    # 锁屏
+    ./desktop/lock.nix
+    # 注销
+    ./desktop/logout.nix
+    # 启动器
+    ./desktop/rofi.nix
+    # 消息
+    ./desktop/notice.nix
+    # 终端
+    ./desktop/terminal.nix
+    # 剪切板
+    ./desktop/clipboard.nix
+    # 主题
+    ./desktop/theme.nix
+
+    # 音频
+    ./software/audio.nix
+    # 视频
+    ./software/video.nix
+    # 图像
+    ./software/image.nix
+
+    # 浏览器
+    ./software/firefox.nix
+    # 邮箱
+    ./software/thunderbird.nix
+    # 即时通讯
+    ./software/im.nix
+  
+    # 下载
+    ./software/download.nix
+    # 管理
+    ./software/manage.nix
+    # 输入法
+    ./software/fcitx.nix
+    # OBS-Studio
+    ./software/obs.nix
+
+    # NeoVim
+    ./software/nvim.nix
+    # VS Code
+    ./software/vscode.nix
+
     # CLI
     ./terminal/cli.nix
-    # 
-    ./terminal/direnv.nix
-    # GNU
-    ./terminal/gnu.nix
     # 工具
     ./terminal/tool.nix
     # ZSH
     ./terminal/zsh.nix
+    # GNU
+    ./terminal/gnu.nix
+    # 开发环境
+    ./terminal/direnv.nix
 
-    # 数学
-    ./study/math.nix
     # 阅读
     ./study/reader.nix
-    # 
-    ./study/tex.nix
     # 编辑
     ./study/writer.nix
+    # Tex
+    ./study/tex.nix
+    # 数学
+    ./study/math.nix
   ];
 }
---
+++ user/common/tool/nix.nix
@@ -0,0 +1,24 @@
+{ outputs, ... }:
+{
+  nixpkgs = {
+    # Overlays
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+
+    # 配置
+    config.allowUnfree = true;
+  };
+
+  # 文档
+  manual = {
+    manpages.enable = false;
+    html.enable = false;
+    json.enable = false;
+  };
+
+  programs.home-manager.enable = true;
+  systemd.user.startServices = "sd-switch";
+}

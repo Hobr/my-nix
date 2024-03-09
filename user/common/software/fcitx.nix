52c625afe85a4ff6c40be3f358801ef5277b3af9
Hobr <mail@hobr.site>
2024-03-09 14:52:46 +0800

    rime fix

--- user/common/software/fcitx.nix
+++ user/common/software/fcitx.nix
@@ -4,12 +4,10 @@
     enabled = "fcitx5";
 
     # Fcitx5
-    fcitx5.addons = with pkgs.unstable; [
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
       fcitx5-gtk
       fcitx5-configtool
-      fcitx5-rime
-      fcitx5-chinese-addons
-      fcitx5-anthy
     ];
   };
 }

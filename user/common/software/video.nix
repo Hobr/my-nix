79c833e5ae03b776a2b5d8ce3757d14b60028bf5
Hobr <mail@hobr.site>
2024-04-04 01:10:14 +0800

    -mpv + vlc

--- user/common/software/video.nix
+++ user/common/software/video.nix
@@ -1,13 +1,7 @@
 {pkgs, ...}: {
-  programs.mpv = {
-    enable = true;
-    bindings = {};
-    defaultProfiles = [];
-    config = {};
-    scripts = with pkgs.mpvScripts; [mpris];
-  };
-
   home.packages = with pkgs.unstable; [
+    vlc
+
     ffmpeg-full
     yt-dlp
     losslesscut-bin

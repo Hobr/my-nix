5fd3db627758f741793ccedd7c4ee9ae44018415
Hobr <mail@hobr.site>
2024-04-04 17:00:44 +0800

    rime-ice to nur

--- user/common/software/fcitx.nix
+++ user/common/software/fcitx.nix
@@ -1,11 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   i18n.inputMethod = {
     enabled = "fcitx5";
 
     # Fcitx5
     fcitx5.addons = with pkgs; [
       (fcitx5-rime.override {
-        rimeDataPkgs = [pkgs.rime-ice];
+        rimeDataPkgs = [inputs.hobr.packages.${pkgs.system}.rime-ice];
       })
       fcitx5-mozc
       fcitx5-chinese-addons

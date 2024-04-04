88e6615b24204dd8517d9890bcc32c3f92d6fcfa
Hobr <mail@hobr.site>
2024-04-04 17:45:28 +0800

    my nur sub

--- system/common/system/nix.nix
+++ system/common/system/nix.nix
@@ -32,7 +32,10 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
-        "https://cache.nixos.org"
+        "https://hobr.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hobr.cachix.org-1:F9LxDwb6IAYpHitjzaCCxeDI3TA4obcgIlEU+7oWXzU="
       ];
     };
 

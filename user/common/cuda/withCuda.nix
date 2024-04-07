055e3725ad34f646b133040d985fe51e1e89b7bf
Hobr <mail@hobr.site>
2024-04-07 23:39:05 +0800

    cuda

--- user/common/cuda/withCuda.nix
+++ user/common/cuda/withCuda.nix
@@ -1,15 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # CUDA
   nixpkgs.config.cudaSupport = true;
 
   programs.btop.package = pkgs.unstable.btop.override {cudaSupport = true;};
 
+  home.sessionVariables = {
+    LD_LIBRARY_PATH = config.home.sessionVariables + "${pkgs.unstable.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib";
+    CUDA_PATH = pkgs.unstable.cudatoolkit;
+    EXTRA_LDFLAGS = "-L/lib -L${pkgs.unstable.linuxPackages.nvidia_x11}/lib";
+    EXTRA_CCFLAGS = "-I/usr/include";
+  };
+
   home.packages = with pkgs.unstable; [
-    (openai-whisper-cpp.override {
-      cudaSupport = true;
-    })
-    (blender.override {
-      cudaSupport = true;
-    })
+    cudatoolkit
+    cudaPackages.cudnn
+    (openai-whisper-cpp.override {cudaSupport = true;})
+    (blender.override {cudaSupport = true;})
   ];
 }

35170e021d06db88a0027bf1d3d2bb513c1568d3
Hobr <mail@hobr.site>
2024-04-10 17:11:26 +0800

    wait for fix

--- user/common/cuda/withCuda.nix
+++ user/common/cuda/withCuda.nix
@@ -5,7 +5,7 @@
   programs.btop.package = pkgs.unstable.btop.override {cudaSupport = true;};
 
   home.packages = with pkgs.unstable; [
-    (openai-whisper-cpp.override {cudaSupport = true;})
-    (blender.override {cudaSupport = true;})
+    # (openai-whisper-cpp.override {cudaSupport = true;})
+    # (blender.override {cudaSupport = true;})
   ];
 }

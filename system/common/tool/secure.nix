632d1deee866e753a99f00a0e1b56edc0583e30a
Hobr <mail@hobr.site>
2024-04-19 21:21:50 +0800

    swaylock

--- system/common/tool/secure.nix
+++ system/common/tool/secure.nix
@@ -18,6 +18,7 @@
       enableGnomeKeyring = true;
       gnupg.enable = true;
     };
+    swaylock = { };
   };
 
   # Gnome Keyring
--- user/common/desktop/lock.nix
+++ user/common/desktop/lock.nix
@@ -3,5 +3,15 @@
     enable = true;
     package = pkgs.swaylock-effects;
     catppuccin.enable = true;
+
+    settings = {
+      screenshots = true;
+      clock = true;
+      fade-in = 0.2;
+
+      indicator = true;
+      indicator-radius = 100;
+      indicator-thickness = 7;
+    };
   };
 }

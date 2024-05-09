897dd815246b0b83548ceea095421358274d6702
Hobr <mail@hobr.site>
2024-05-09 18:50:37 +0800

    flake-utils

--- devshell/clojure.nix
+++ devshell/clojure.nix
@@ -6,27 +6,23 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    let
-      # Java版本
-      javaVersion = 20;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Java版本
+        javaVersion = 20;
 
-      overlays = [
-        (final: prev: rec {
-          jdk = prev."jdk${toString javaVersion}";
-          boot = prev.boot.override { inherit jdk; };
-          clojure = prev.clojure.override { inherit jdk; };
-          leiningen = prev.leiningen.override { inherit jdk; };
-        })
-      ];
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default =
-          pkgs.mkShell { packages = with pkgs; [ boot clojure leiningen ]; };
+        overlays = [
+          (final: prev: rec {
+            jdk = prev."jdk${toString javaVersion}";
+            boot = prev.boot.override { inherit jdk; };
+            clojure = prev.clojure.override { inherit jdk; };
+            leiningen = prev.leiningen.override { inherit jdk; };
+          })
+        ];
+
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShell = with pkgs;
+          mkShell rec { packages = with pkgs; [ boot clojure leiningen ]; };
       });
-    };
 }
--- devshell/flutter.nix
+++ devshell/flutter.nix
@@ -15,12 +15,15 @@
             allowUnfree = true;
           };
         };
+
         buildToolsVersion = "34.0.0";
+
         androidComposition = pkgs.androidenv.composeAndroidPackages {
           buildToolsVersions = [ buildToolsVersion "28.0.3" ];
           platformVersions = [ "34" "28" ];
           abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
         };
+
         androidSdk = androidComposition.androidsdk;
       in {
         devShell = with pkgs;

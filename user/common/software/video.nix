7b4141a64f659b24fe541340c2cbb97fbfa22184
Hobr <mail@hobr.site>
2024-05-10 18:16:18 +0800

    realesrgan

--- user/common/software/video.nix
+++ user/common/software/video.nix
@@ -5,8 +5,15 @@
   home.packages = with pkgs; [
     ffmpeg
     yt-dlp
+    # 动画
     manim
+    # 编辑器
     olive-editor
+    # 字幕
     inputs.hobr.packages.${pkgs.system}.aegisub
+    # 补帧
+    # inputs.hobr.packages.${pkgs.system}.rife-ncnn-vulkan
+    # 超分
+    realesrgan-ncnn-vulkan
   ];
 }

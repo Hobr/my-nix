f49de8285b4133610489397ed3f43e4fe8a82a59
Hobr <mail@hobr.site>
2024-07-04 17:01:08 +0800

    app

--- user/common/software/video.nix
+++ user/common/software/video.nix
@@ -24,16 +24,11 @@
     ffmpeg
     yt-dlp
     # 动画
-    # manim
+    manim
     # 剪辑后期
-    # natron
-    # olive-editor
+    natron
+    olive-editor
     # 字幕
     inputs.hobr.packages.${pkgs.system}.aegisub
-    # 补帧
-    # inputs.hobr.packages.${pkgs.system}.rife-ncnn-vulkan
-    # svp
-    # 超分
-    # inputs.hobr.packages.${pkgs.system}.real-esrgan
   ];
 }

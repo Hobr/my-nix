23cc6ed9a86ab5a5fbc842a20880f857b5e2c4d2
Hobr <mail@hobr.site>
2024-07-16 00:27:35 +0800

    format

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,7 +7,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):

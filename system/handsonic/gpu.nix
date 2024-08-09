6d8f5a933c66833fe3b276156695866fa83b9a5d
Hobr <mail@hobr.site>
2024-08-09 17:38:35 +0800

    fix nvidia

--- system/handsonic/gpu.nix
+++ system/handsonic/gpu.nix
@@ -32,7 +32,7 @@
   # NVIDIA驱动
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     open = true;
     modesetting.enable = true;
     powerManagement.enable = true;

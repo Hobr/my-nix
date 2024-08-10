5d23234386e27de01c13bf323514333c0752d845
Hobr <mail@hobr.site>
2024-08-10 15:25:35 +0800

    +discord

--- user/common/software/im.nix
+++ user/common/software/im.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     telegram-desktop
-    fractal
+    discord
     (qq.override (old: {
       # Wayland Fcitx5
       commandLineArgs = (old.commandLineArgs or [ ]) ++ [ "--enable-wayland-ime" ];

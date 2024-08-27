7d5e27b787d03e50bcba5f05dc5900ed5eb4d595
Hobr <mail@hobr.site>
2024-08-27 16:28:36 +0800

    nvidia

--- module/system/gpu/nvidia.nix
+++ module/system/gpu/nvidia.nix
@@ -40,19 +40,7 @@ in
     # NVIDIA驱动
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "560.31.02";
-        sha256_64bit = "sha256-0cwgejoFsefl2M6jdWZC+CKc58CqOXDjSi4saVPNKY0=";
-        sha256_aarch64 = "sha256-m7da+/Uc2+BOYj6mGON75h03hKlIWItHORc5+UvXBQc=";
-        openSha256 = "sha256-X5UzbIkILvo0QZlsTl9PisosgPj/XRmuuMH+cDohdZQ=";
-        settingsSha256 = "sha256-A3SzGAW4vR2uxT1Cv+Pn+Sbm9lLF5a/DGzlnPhxVvmE=";
-        persistencedSha256 = "sha256-BDtdpH5f9/PutG3Pv9G4ekqHafPm3xgDYdTcQumyMtg=";
-        preInstall = ''
-          rm -f ./libnvidia-egl-wayland.so*
-          cp ${pkgs.egl-wayland}/lib/libnvidia-egl-wayland.so.1.* .
-          chmod 777 ./libnvidia-egl-wayland.so.1.*
-        '';
-      };
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
       open = true;
       modesetting.enable = true;
       powerManagement.enable = true;

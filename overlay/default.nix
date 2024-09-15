23d63f0d43893d530af7308f71a330ab7a369f5a
Hobr <mail@hobr.site>
2024-09-15 23:03:54 +0800

    fix cuda

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,10 +1,13 @@
 { inputs, ... }:
 {
   modifications = self: super: {
-    opencv = super.opencv.override {
+    opencv4 = super.opencv4.override {
       cudaPackages = super.cudaPackages_12_3;
     };
     frei0r = super.frei0r.override {
+      cudaPackages = super.cudaPackages_12_3;
+    };
+    mlt = super.mlt.override {
       cudaPackages = super.cudaPackages_12_3;
     };
   };

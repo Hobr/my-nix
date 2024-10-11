c329a6176bfe5bb408a4323cb92b98875a8c8478
Hobr <mail@hobr.site>
2024-10-11 20:21:28 +0800

    rm opencv cuda mod

--- overlay/default.nix
+++ overlay/default.nix
@@ -2,15 +2,8 @@
 {
   additions = final: _prev: import ../pkg final.pkgs;
 
-  modifications = final: prev: {
-    opencv4 = prev.opencv4.override {
-      cudaPackages = prev.cudaPackages_12_3;
+  modifications =
+    final: prev:
+    {
     };
-    frei0r = prev.frei0r.override {
-      cudaPackages = prev.cudaPackages_12_3;
-    };
-    mlt = prev.mlt.override {
-      cudaPackages = prev.cudaPackages_12_3;
-    };
-  };
 }

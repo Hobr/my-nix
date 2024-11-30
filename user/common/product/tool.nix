e85f7d02fec3751c7ceb9e3fae9e65a6720278a6
Hobr <mail@hobr.site>
2024-11-30 15:55:11 +0800

    useless

--- user/common/product/tool.nix
+++ user/common/product/tool.nix
@@ -6,18 +6,14 @@
     # 虚拟机
     virt-manager
     # 开发
-    gitkraken
     hoppscotch
-    frida-tools
     # 二进制
     ghex
-    imhex
     # Nix
     nurl
     nix-init
     nix-update
     nixfmt-rfc-style
     nixpkgs-review
-    prefetch-npm-deps
   ];
 }

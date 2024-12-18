f6203d50375f542a9f4f164a6d57dc8a30a24e25
Hobr <mail@hobr.site>
2024-12-18 17:10:06 +0800

    qq wayland

--- module/home/web/im.nix
+++ module/home/web/im.nix
@@ -17,10 +17,7 @@ in
     home.packages = with pkgs; [
       telegram-desktop
       wechat-uos
-      (qq.override (old: {
-        # Wayland Fcitx5
-        commandLineArgs = (old.commandLineArgs or [ ]) ++ [ "--enable-wayland-ime" ];
-      }))
+      qq
     ];
   };
 }

7ed2f54cd7394d9d8d2d71b61bb8630317dac3b7
Hobr <mail@hobr.site>
2024-12-18 15:37:25 +0800

    dev.util & pro.electron

---
+++ module/home/dev/util.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.home.dev.util;
+in
+{
+  options.home.dev.util.enable = mkEnableOption "util";
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      # 虚拟机
+      virt-manager
+      # HTTP
+      hoppscotch
+      # 二进制
+      ghex
+    ];
+  };
+}
---
+++ module/home/pro/electron.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib;
+let
+  cfg = config.home.pro.electron;
+in
+{
+  options.home.pro.electron.enable = mkEnableOption "electron";
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      # 电路设计
+      stable.kicad-small
+      stable.kikit
+      librepcb
+      klayout
+      circt
+      # 仿真
+      yosys
+      inputs.nix-xilinx.packages.${pkgs.system}.vivado
+      qucs-s
+      # 模拟器
+      ripes
+      qtrvsim
+      # Verilog
+      verilog
+      verilator
+      verible
+      # 波形
+      gtkwave
+    ];
+  };
+}

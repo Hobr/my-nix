b875bcf8af25be485203c5c5ab8e33fb52ad43a4
Hobr <mail@hobr.site>
2024-12-18 15:18:29 +0800

    media audio

---
+++ module/home/media/audio.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.home.media.audio;
+in
+{
+  options.home.media.audio.enable = mkEnableOption "audio";
+
+  config = mkIf cfg.enable {
+    # MPD
+    services.mpd = {
+      enable = true;
+      extraConfig = "";
+    };
+
+    # MPD歌词
+    services.mpd-mpris = {
+      enable = true;
+      mpd.useLocal = true;
+    };
+
+    # MPD客户端
+    programs.ncmpcpp = {
+      enable = true;
+      bindings = [ ];
+      settings = { };
+    };
+
+    home.packages = with pkgs; [
+      # 音乐播放器
+      amberol
+    ];
+  };
+}

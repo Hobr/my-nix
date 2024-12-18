523d5ccd3642b2da1079e1236f922e728c9e4381
Hobr <mail@hobr.site>
2024-12-18 15:15:59 +0800

    util.audio

---
+++ module/home/util/audio.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  options,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.home.util.audio;
+in
+{
+  options.home.util.audio.enable = mkEnableOption "audio";
+
+  config = mkIf cfg.enable {
+    # Playctl
+    services.playerctld.enable = true;
+
+    home.packages = with pkgs; [
+      playerctl
+      # GStreamer
+      gst_all_1.gstreamer
+      gst_all_1.gstreamer.dev
+      gst_all_1.gst-libav
+      gst_all_1.gst-vaapi
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-plugins-rs
+      gst_all_1.gstreamermm
+      gst_all_1.gst-editing-services
+    ];
+  };
+}
--- user/common/software/audio.nix
+++ user/common/software/audio.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }:
 {
-  # Playctl
-  services.playerctld = {
-    enable = true;
-  };
-
   # MPD
   services.mpd = {
     enable = true;
@@ -27,20 +22,7 @@
   home.packages = with pkgs; [
     # 音乐播放器
     amberol
-    playerctl
     # 音频编辑
     audacity
-    # GStreamer
-    gst_all_1.gstreamer
-    gst_all_1.gstreamer.dev
-    gst_all_1.gst-libav
-    gst_all_1.gst-vaapi
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-plugins-rs
-    gst_all_1.gstreamermm
-    gst_all_1.gst-editing-services
   ];
 }

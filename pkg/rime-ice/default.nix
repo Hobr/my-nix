0cd34e5fec624f3f3453e1aa7249e81c3d8751a9
Hobr <mail@hobr.site>
2024-12-29 16:37:38 +0800

    rime-ice 1228

--- pkg/rime-ice/default.nix
+++ pkg/rime-ice/default.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "rime-ice";
-  version = "2024.09.25";
+  version = "2024.12.28";
 
   src = fetchFromGitHub {
     owner = "iDvel";
     repo = "rime-ice";
-    rev = "2024.09.25";
-    hash = "sha256-R2K2LNycXqsUxXvMMq5fcQJUhDnhQMcTlvryMLeiKH0=";
+    rev = "main";
+    hash = "sha256-tFeGkY0sg3ZmrDidIC9MRndQQnCLPtimkKZ6FwUq2Is=";
   };
 
   installPhase = ''
@@ -23,7 +23,9 @@ stdenvNoCC.mkDerivation {
   meta = with lib; {
     description = "Rime 配置：雾凇拼音 | 长期维护的简体词库";
     homepage = "https://github.com/iDvel/rime-ice";
-    license = licenses.gpl3;
+    license = licenses.gpl3Only;
+    maintainers = [ ];
+    mainProgram = "rime-ice";
     platforms = platforms.all;
   };
 }

dc5a5ce2cc5d21da1d4b77609352c632ff0cdc98
Hobr <mail@hobr.site>
2025-04-14 15:29:41 +0800

    nixos-unstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # 软件源
     ## 官方
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     ## 官方稳定
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Chaotic

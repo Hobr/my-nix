7291e452820bb9c020cc565d727959945d5e4d03
Hobr <mail@hobr.site>
2025-07-02 14:34:05 +0800

    Update

--- pkg/gemini-cli/default.nix
+++ pkg/gemini-cli/default.nix
@@ -10,21 +10,23 @@
   jq,
   nix-prefetch-github,
   prefetch-npm-deps,
+  gitUpdater,
 }:
 
 buildNpmPackage (finalAttrs: {
   pname = "gemini-cli";
-  version = "0.1.7";
+  version = "0.1.9";
+
   src = fetchFromGitHub {
     owner = "google-gemini";
     repo = "gemini-cli";
-    tag = "v0.1.7";
-    hash = "sha256-DAenod/w9BydYdYsOnuLj7kCQRcTnZ81tf4MhLUug6c=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-P8ZjUMkgl/AH1k4H6jOMg2hVXY3kqPi68lPAYvVcwvc=";
   };
 
   npmDeps = fetchNpmDeps {
     inherit (finalAttrs) src;
-    hash = "sha256-otogkSsKJ5j1BY00y4SRhL9pm7CK9nmzVisvGCDIMlU=";
+    hash = "sha256-d1PQhXk9Nz0EduemwuLfS0lNOR+GwSc53wBn/k/l5tU=";
   };
 
   preConfigure = ''
@@ -55,33 +57,7 @@ buildNpmPackage (finalAttrs: {
     chmod +x "$out/bin/gemini"
   '';
 
-  passthru.updateScript = lib.getExe (writeShellApplication {
-    name = "gemini-cli-update-script";
-    runtimeInputs = [
-      cacert
-      curl
-      gnused
-      jq
-      nix-prefetch-github
-      prefetch-npm-deps
-    ];
-    text = ''
-      latest_version=$(curl -s "https://raw.githubusercontent.com/google-gemini/gemini-cli/main/package-lock.json" | jq -r '.version')
-      latest_rev=$(curl -s "https://api.github.com/repos/google-gemini/gemini-cli/commits/main" | jq -r '.sha')
-
-      src_hash=$(nix-prefetch-github google-gemini gemini-cli --rev "$latest_rev" | jq -r '.hash')
-
-      temp_dir=$(mktemp -d)
-      curl -s "https://raw.githubusercontent.com/google-gemini/gemini-cli/$latest_rev/package-lock.json" > "$temp_dir/package-lock.json"
-      npm_deps_hash=$(prefetch-npm-deps "$temp_dir/package-lock.json")
-      rm -rf "$temp_dir"
-
-      sed -i "s|version = \".*\";|version = \"$latest_version\";|" "pkgs/by-name/ge/gemini-cli/package.nix"
-      sed -i "s|rev = \".*\";|rev = \"$latest_rev\";|" "pkgs/by-name/ge/gemini-cli/package.nix"
-      sed -i "/src = fetchFromGitHub/,/};/s|hash = \".*\";|hash = \"$src_hash\";|" "pkgs/by-name/ge/gemini-cli/package.nix"
-      sed -i "/npmDeps = fetchNpmDeps/,/};/s|hash = \".*\";|hash = \"$npm_deps_hash\";|" "pkgs/by-name/ge/gemini-cli/package.nix"
-    '';
-  });
+  passthru.updateScript = gitUpdater { };
 
   meta = {
     description = "AI agent that brings the power of Gemini directly into your terminal";

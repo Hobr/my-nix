482d93ce88203547d5999091e90fc1ed95023e5b
Hobr <mail@hobr.site>
2025-08-23 00:35:59 +0800

    useless

--- module/system/program/ssh.nix
+++ module/system/program/ssh.nix
@@ -27,8 +27,6 @@ in
       };
     };
 
-    services.fail2ban.enable = true;
-
     users.users."${cfg.user}".openssh.authorizedKeys.keys = [
       # Handsonic
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8VGTWsM1bXg5RUcaEvr+4ualB0DJnDXbB3nNQmMd1p0yXqqZOQ3CfYpgSl23Oo3FgflNSbibTk/8g0wPBjalBFSNyZ/4DfC6RWD3YVuqIZCPkGGPD37JiIGHcw1s2tPGojonIytTcHc/xF+zG72FcVtkcOSLXM2xkk9oEZ/ElJe0SQ61J5uBvLJm4pKftg9a9sGRN75VqGvzbDKzKWdOq4gLq/q151NUAGSryirfXTmPZiGZS7jg9O2zUrdC59gE0y2nTwLWXW3MrMj/qAQuLpge8cNhCaUw3KLYwYxtOUOOvNfDtZculf6R81Em/XjMOCdJ0DFhlcv1uCYuAgMU35yqVbyPaAJQvb+A4uGtKxx0AoVghBLaWxoMUXLQCUyr56q1Dpjg+QwXwkRXFssLQefMR+PojyQKYnFIE/ikNVj83iOA9C3rVLoiyV743xfum5QtHj0fQN0c+1sWiSRx7vr9dIcFpxRx9K06Mf7S9aJ+7t3YoXr1XYYc9v8XmiLc= mail@hobr.site"

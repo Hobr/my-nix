5bd7868c53afcf17b75f59b3a287909a02466988
Hobr <mail@hobr.site>
2026-02-22 15:11:18 +0800

    fix whisperx

--- overlay/default.nix
+++ overlay/default.nix
@@ -6,19 +6,6 @@
   additions = final: prev: import ../pkg final.pkgs;
 
   modifications = final: prev: {
-    pythonPackagesExtensions = (prev.pythonPackagesExtensions or [ ]) ++ [
-      (python-final: python-prev: {
-        whisperx = python-prev.whisperx.overridePythonAttrs (old: {
-          patches = (old.patches or [ ]) ++ [
-            ./whisperx.patch
-          ];
-
-          propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
-            python-final.omegaconf
-          ];
-        });
-      })
-    ];
   };
 
   stable-package = final: _prev: {

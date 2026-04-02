9be368d35ee82665fa91ab4ccdd09f9c478dd705
Hobr <mail@hobr.site>
2026-04-02 14:26:53 +0800

    wallpaper

--- user/kanade.nix
+++ user/kanade.nix
@@ -23,12 +23,8 @@
 
   # 壁纸
   stylix.image = pkgs.fetchurl {
-    /*
-      url = "https://images5.alphacoders.com/941/941673.png";
-      hash = "sha256-nknMKEWpm2pyHbdW83x0/ixPmfbpWRfGt8Z12YYPAlE=";
-    */
-    url = "https://images.alphacoders.com/499/499786.png";
-    hash = "sha256-zblZCkXLc5rt4bimLpWfbj+mYzaVL55L+D/R5HQhjO8=";
+    url = "https://images5.alphacoders.com/941/941673.png";
+    hash = "sha256-nknMKEWpm2pyHbdW83x0/ixPmfbpWRfGt8Z12YYPAlE=";
   };
 
   nixpkgs.config.cudaSupport = true;

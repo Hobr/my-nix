36ca3a139b1ef8c047b983850d2b8200cb0f816f
Hobr <mail@hobr.site>
2026-04-02 20:46:04 +0800

    zellij config

--- module/home/util/zellij.nix
+++ module/home/util/zellij.nix
@@ -19,12 +19,15 @@ in
       exitShellOnExit = true;
 
       settings = {
+        # 主题
+        theme = "catppuccin-macchiato";
         # 简化UI
         simplified_ui = false;
+
         # 自动布局调整
         auto_layout = true;
         # 显示窗格边框
-        pane_frames = true;
+        pane_frames = false;
         # 启用视觉提示
         visual_bell = true;
         # 启用样式化下划线
@@ -33,14 +36,19 @@ in
         # 启动时Tip
         show_startup_tips = false;
         # 默认模式
-        default_mode = "normal";
+        default_mode = "locked";
         # 默认shell
         default_shell = "zsh";
         # Kitty键盘协议
         support_kitty_keyboard_protocol = true;
 
-        # 鼠标复制
+        # 收到关闭信号时
+        on_force_close = "quit";
+        # 选中复制
         copy_on_select = true;
+        # 复制命令
+        copy_command = "wl-copy";
+
         # 启用鼠标
         mouse_mode = true;
         # 鼠标悬停效果
@@ -49,5 +57,7 @@ in
         focus_follows_mouse = false;
       };
     };
+
+    stylix.targets.zellij.enable = false;
   };
 }

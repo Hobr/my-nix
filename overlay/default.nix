b0d9e0266ddb7c0ffc040f77462944529dc31501
Hobr <mail@hobr.site>
2026-05-24 18:42:21 +0800

    useless

--- overlay/default.nix
+++ overlay/default.nix
@@ -6,19 +6,6 @@
   additions = final: prev: import ../pkg final.pkgs;
 
   modifications = final: prev: {
-    # 临时应用 https://github.com/NixOS/nixpkgs/pull/522705
-    # 修复 python3.13-jedi-language-server 与 jedi 0.20.0 不兼容导致的构建失败
-    # 影响 vscode-extension-ms-python-python 的安装
-    python313 = prev.python313.override (old: {
-      packageOverrides = prev.lib.composeExtensions (old.packageOverrides or (_: _: { })) (
-        pyfinal: pyprev: {
-          jedi-language-server = pyprev.jedi-language-server.overrideAttrs (oldAttrs: {
-            pythonRelaxDeps = (oldAttrs.pythonRelaxDeps or [ ]) ++ [ "jedi" ];
-          });
-        }
-      );
-    });
-
     linuxPackages = final.pkgs.linuxPackages_latest;
   };
 

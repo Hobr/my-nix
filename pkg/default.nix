09b11bab67082f0aae5492320906e1d4051c28d2
Hobr <mail@hobr.site>
2026-07-03 11:47:19 +0800

    rm trellis

--- pkg/default.nix
+++ pkg/default.nix
@@ -1,5 +1,4 @@
 pkgs: {
-  trellis = pkgs.callPackage ./trellis { };
   dirhash = pkgs.python3Packages.callPackage ./dirhash { };
   harbor = pkgs.python3Packages.callPackage ./harbor {
     dirhash = pkgs.python3Packages.callPackage ./dirhash { };

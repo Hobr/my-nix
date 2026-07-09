17d5ecb71da2673c599f61dfb0c200c6ac1743f5
Hobr <mail@hobr.site>
2026-07-09 12:30:47 +0800

    rm gst_all_1.gst-vaapi

--- module/home/util/audio.nix
+++ module/home/util/audio.nix
@@ -22,7 +22,6 @@ in
       gst_all_1.gstreamer
       gst_all_1.gstreamer.dev
       gst_all_1.gst-libav
-      gst_all_1.gst-vaapi
       gst_all_1.gst-plugins-base
       gst_all_1.gst-plugins-good
       gst_all_1.gst-plugins-bad

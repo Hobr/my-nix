1148e81a584e52fb9c2e37da65378b4dde266ab6
Hobr <mail@hobr.site>
2026-07-17 18:29:16 +0800

    rm harbor local

--- pkg/default.nix
+++ pkg/default.nix
@@ -1,6 +1,1 @@
-pkgs: {
-  dirhash = pkgs.python3Packages.callPackage ./dirhash { };
-  harbor = pkgs.python3Packages.callPackage ./harbor {
-    dirhash = pkgs.python3Packages.callPackage ./dirhash { };
-  };
-}
+pkgs: { }

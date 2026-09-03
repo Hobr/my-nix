1910d662a69634114ece07ed087e16741ef4500d
Hobr <mail@hobr.site>
2026-09-03 13:35:54 +0800

    lanzaboote

--- module/system/boot/secure-boot.nix
+++ module/system/boot/secure-boot.nix
@@ -16,9 +16,26 @@ in
     environment.systemPackages = with pkgs; [ sbctl ];
 
     boot.loader.systemd-boot.enable = mkForce false;
+
     boot.lanzaboote = {
       enable = true;
+      autoGenerateKeys.enable = true;
+      configurationLimit = 8;
       pkiBundle = "/etc/secureboot";
+
+      autoEnrollKeys = {
+        enable = true;
+        autoReboot = true;
+      };
+
+      measuredBoot = {
+        enable = true;
+        pcrs = [
+          0
+          4
+          7
+        ];
+      };
     };
   };
 }
